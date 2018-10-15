000ca027b0b1285924a87e9c12492b380e7fad5f
Roy Veshovda <roy@veshovda.no>
2018-10-15 14:21:28 +0200

    feat: P5 first part: implement contracts and tests

---
+++ P5/smart_contracts/contracts/StarNotary.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.4.23;
+
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+
+contract StarNotary is ERC721 { 
+    struct Star { 
+        string name;
+        string dec;
+        string mag;
+        string cent;      
+        string story;
+    }
+
+    mapping(uint256 => Star) public tokenIdToStar; 
+    mapping(uint256 => uint256) public starsForSale;
+    mapping(bytes32 => uint256) public starCoordinatesToTokenId;
+
+    function createStar(string _name, string _dec, string _mag, string _cent, string _story, uint256 _tokenId) public { 
+        require(checkIfStarExist(_dec, _mag, _cent) == false, "This start does already exists.");
+
+        Star memory newStar = Star(_name, _dec, _mag, _cent, _story);
+        bytes32 starCoordinates = starCoordinatesToSingleString(_dec, _mag, _cent);
+        starCoordinatesToTokenId[starCoordinates] = _tokenId;
+        
+        tokenIdToStar[_tokenId] = newStar;
+
+        _mint(msg.sender, _tokenId);
+    }
+
+    function checkIfStarExist(string dec, string mag, string cent) public view returns (bool) {
+        bytes32 starCoordinates = starCoordinatesToSingleString(dec, mag, cent);
+        return starCoordinatesToTokenId[starCoordinates] != 0;
+    }
+
+    function starCoordinatesToSingleString(string dec, string mag, string cent) private pure returns (bytes32) {
+        bytes memory coordinates = bytes(string(abi.encodePacked(dec, mag, cent)));
+        bytes32 coordinatesHash = keccak256(coordinates);
+        return coordinatesHash;
+    }
+
+    function tokenIdToStarInfo(uint256 _tokenId) public view returns(string name, string story, string deg, string mag, string cent) {
+        Star memory s = tokenIdToStar[_tokenId];
+        return (s.name, s.story, s.dec, s.mag, s.cent);
+    }
+
+    function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
+        require(this.ownerOf(_tokenId) == msg.sender);
+
+        starsForSale[_tokenId] = _price;
+    }
+
+    function buyStar(uint256 _tokenId) public payable { 
+        require(starsForSale[_tokenId] > 0);
+        
+        uint256 starCost = starsForSale[_tokenId];
+        address starOwner = this.ownerOf(_tokenId);
+        require(msg.value >= starCost);
+
+        _removeTokenFrom(starOwner, _tokenId);
+        _addTokenTo(msg.sender, _tokenId);
+        
+        starOwner.transfer(starCost);
+
+        if(msg.value > starCost) { 
+            msg.sender.transfer(msg.value - starCost);
+        }
+    }
+}
---
+++ P5/smart_contracts/test/StarNotaryTest.js
@@ -0,0 +1,136 @@
+const StarNotary = artifacts.require('StarNotary')
+
+contract('StarNotary', accounts => { 
+
+    beforeEach(async function() { 
+        this.contract = await StarNotary.new({from: accounts[0]})
+    })
+    
+    describe('can create a star', () => { 
+        it('can create a star and get its info back', async function () { 
+            
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', 1, {from: accounts[0]})
+
+            var star = await this.contract.tokenIdToStarInfo(1)
+            assert.equal(star[0], 'awesome star!')
+            assert.equal(star[1], 'Too long to tell')
+            assert.equal(star[2], '123')
+            assert.equal(star[3], '456')
+            assert.equal(star[4], '789')
+        })
+
+        it('can create a second star with different coordinates (dec) and get its info back', async function () { 
+            
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', 1, {from: accounts[0]})
+            await this.contract.createStar('awesome star!', '1234', '456', '789', 'Too long to tell', 2, {from: accounts[0]})
+
+            var star = await this.contract.tokenIdToStarInfo(2)
+            assert.equal(star[0], 'awesome star!')
+            assert.equal(star[1], 'Too long to tell')
+            assert.equal(star[2], '1234')
+            assert.equal(star[3], '456')
+            assert.equal(star[4], '789')
+        })
+
+        it('can create a second star with different coordinates (mag) and get its info back', async function () { 
+            
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', 1, {from: accounts[0]})
+            await this.contract.createStar('awesome star!', '123', '4567', '789', 'Too long to tell', 2, {from: accounts[0]})
+
+            var star = await this.contract.tokenIdToStarInfo(2)
+            assert.equal(star[0], 'awesome star!')
+            assert.equal(star[1], 'Too long to tell')
+            assert.equal(star[2], '123')
+            assert.equal(star[3], '4567')
+            assert.equal(star[4], '789')
+        })
+
+        it('can create a second star with different coordinates (cent) and get its info back', async function () { 
+            
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', 1, {from: accounts[0]})
+            await this.contract.createStar('awesome star!', '123', '456', '7890', 'Too long to tell', 2, {from: accounts[0]})
+
+            var star = await this.contract.tokenIdToStarInfo(2)
+            assert.equal(star[0], 'awesome star!')
+            assert.equal(star[1], 'Too long to tell')
+            assert.equal(star[2], '123')
+            assert.equal(star[3], '456')
+            assert.equal(star[4], '7890')
+        })
+    })
+
+
+    describe('can create a star and check if it exists', () => { 
+        it('star does not exists if not created', async function () {            
+            var starExists = await this.contract.checkIfStarExist('123', '456', '789')
+            assert.equal(starExists, false)
+        })
+
+        it('can create a star and it does exist', async function () {            
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', 1, {from: accounts[0]})
+
+            var starExists = await this.contract.checkIfStarExist('123', '456', '789')
+            assert.equal(starExists, true)
+        })
+    })
+
+    describe('cannot create a star', () => { 
+        it('cannot create a star with same coordinates as an existing one', async function () { 
+            
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', 1, {from: accounts[0]})
+            
+            await expectThrow(this.contract.createStar('awesome star 2!', '123', '456', '789', 'Too long to tell 2', 2, {from: accounts[0]}))
+        })
+    })
+
+    describe('buying and selling stars', () => { 
+        let user1 = accounts[1]
+        let user2 = accounts[2]
+        let randomMaliciousUser = accounts[3]
+        
+        let starId = 1
+        let starPrice = web3.toWei(.01, "ether")
+
+        beforeEach(async function () { 
+            await this.contract.createStar('awesome star!', '123', '456', '789', 'Too long to tell', starId, {from: user1})    
+        })
+
+        it('user1 can put up their star for sale', async function () { 
+            assert.equal(await this.contract.ownerOf(starId), user1)
+            await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+            
+            assert.equal(await this.contract.starsForSale(starId), starPrice)
+        })
+
+        describe('user2 can buy a star that was put up for sale', () => { 
+            beforeEach(async function () { 
+                await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+            })
+
+            it('user2 is the owner of the star after they buy it', async function() { 
+                await this.contract.buyStar(starId, {from: user2, value: starPrice, gasPrice: 0})
+                assert.equal(await this.contract.ownerOf(starId), user2)
+            })
+
+            it('user2 ether balance changed correctly', async function () { 
+                let overpaidAmount = web3.toWei(.05, 'ether')
+                const balanceBeforeTransaction = web3.eth.getBalance(user2)
+                await this.contract.buyStar(starId, {from: user2, value: overpaidAmount, gasPrice: 0})
+                const balanceAfterTransaction = web3.eth.getBalance(user2)
+
+                assert.equal(balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice)
+            })
+        })
+    })
+})
+
+var expectThrow = async function(promise) { 
+    try { 
+        await promise
+    } catch (error) { 
+        assert.exists(error)
+        return
+    }
+
+    assert.fail('Expected an error but didnt see one!')
+}
